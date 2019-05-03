0f12586d23d132f0af450dc639fc807d55226981
M-garrigan <mgarrigan99@gmail.com>
2019-05-03 07:42:50 -0400

    Add NavBar and Hero components.
    The NavBar component will have the basic menu options for searching
    data. And the hero will be the graphical landing page that will be
    transformed into the actual search and google map display area.

--- src/main.js
+++ src/main.js
@@ -1,12 +1,39 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
+
+import NavBar from './components/NavBar.js';
+import Hero from './components/Hero.js';
 
 class App extends Component {
+  state = {
+    data: ""
+  }
+
+  retrieveData = (event) => {
+    event.preventDefault();
+    axios.get('/data')
+      .then(response => {
+        console.log('daCo: ', response.data)
+        this.setState({
+          data: response.data.data
+        })
+      })
+  }
 
   render() {
     return (
-      <div>
-        Hello Reacty World.
+      <div class="nyde-wrapper">
+        <NavBar 
+          retrieveData={this.retrieveData}
+        />
+        <Hero />
+        {
+          this.state.data 
+            ? (
+              this.state.data.map(item => (<p key={item.npi}>{JSON.stringify(item)}</p>))
+            ) : ""
+        }
       </div>
     )
   }
