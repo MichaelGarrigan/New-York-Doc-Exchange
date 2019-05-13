3338c9a89aafde6da065410c233c2be185a20a40
M-garrigan <mgarrigan99@gmail.com>
2019-05-13 19:58:32 -0400

    Changed the Search button to be a floating Material UI style button.
    This search button brings up the input options for the type of doctor
    to be selected.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,21 +1,46 @@
 import React from 'react';
+import SearchBar from './SearchBar.js';
+
+import { withStyles } from '@material-ui/core/styles';
+import Fab from '@material-ui/core/Fab';
+import SearchIcon from '@material-ui/icons/Search';
+
 import '../styles/NavBar.css';
 
-const NavBar = props => (
-  <nav>
-    <div class="nav-title">New York Doc Exchange</div>
-    <div class="nav-bottom-row">
-      <button>Search</button>
-      <button>Map</button>
-      <button
-        onClick={
-          event => {
-            props.retrieveData(event);
-          }
-        }
-      >Data</button>
-    </div>
-  </nav>
-);
+const styles = theme => ({
+  fab: {
+    margin: theme.spacing.unit,
+  },
+  extendedIcon: {
+    marginRight: theme.spacing.unit,
+  }
+});
 
-export default NavBar;
+const NavBar = props => {
+  const { classes } = props;
+  
+  return (
+    <nav>
+      <div class="nav-top-row">
+        <div class="nav-title">New York Doc Exchange</div>
+        <Fab 
+          aria-label="Search"
+          color="secondary" 
+          className={classes.fab} 
+          onClick={event => {props.toggleSearchNav(event)}}
+          variant="extended"
+        >
+          <SearchIcon />
+          Search
+        </Fab>
+      </div>
+      {
+        props.showSearchNav
+          ? <SearchBar />
+          : ""
+      }
+    </nav>
+  );
+};
+
+export default withStyles(styles)(NavBar);
