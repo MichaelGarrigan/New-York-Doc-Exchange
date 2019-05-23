224618b900c67be9f85691f254a19c87c03892f1
M-garrigan <mgarrigan99@gmail.com>
2019-05-22 21:15:46 -0400

    Removed the material-ui portion and replaced the icon with my own icon.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,25 +1,8 @@
 import React from 'react';
 
-import { withStyles } from '@material-ui/core/styles';
-import Fab from '@material-ui/core/Fab';
-import SearchIcon from '@material-ui/icons/Search';
-
 import '../styles/NavBar.css';
 
-const styles = theme => ({
-  fab: {
-    margin: theme.spacing.unit,
-  },
-  extendedIcon: {
-    marginRight: theme.spacing.unit,
-  }
-});
-
-
-const NavBar = props => {
-  const { classes } = props;
-  
-  return (
+const NavBar = props => (
     <nav className="navbar-wrapper">
 
       <div class="nav-title"
@@ -28,19 +11,26 @@ const NavBar = props => {
         New York Doc Exchange
       </div>
 
-      <Fab 
-        aria-label="Search"
-        color="secondary" 
-        className={classes.fab} 
+      <button
+        className="navbar-search-button"
         onClick={ () => props.switchMainView('search') }
-        variant="extended"
       >
-        <SearchIcon />
-        Search
-      </Fab>
-    
+        <div className="navbar-search-flex">
+          <svg 
+            className="icon-search"
+            viewBox="0 0 25 25"
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg" 
+          >
+            <path
+              d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"
+            />
+          </svg>
+          <span>Search</span>
+        </div>
+      </button>
     </nav>
-  );
-};
+);
 
-export default withStyles(styles)(NavBar);
+
+export default NavBar;
