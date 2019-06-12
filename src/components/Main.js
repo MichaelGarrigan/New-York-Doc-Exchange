6ac53ebe9e84b244de5048b2e796263709041af1
M-garrigan <mgarrigan99@gmail.com>
2019-06-12 17:36:34 -0400

    Removed Google map logic and ui to its own Component called 'Map'.

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,43 +2,21 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 import SideBar from './SideBar.js';
-import GoogleMapReact from 'google-map-react';
-import { API_KEY_GEO } from '../server/helpers/config.js';
-
+import Map from './Map.js';
 import '../styles/Main.css';
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+const Main = props => {
 
-class Main extends Component {
+  return (
+    <main className="main-wrapper">
+      <SideBar doctorData={props.doctorData} />
 
-  render() {
-    return (
-      <main className="main-wrapper">
-        <SideBar doctorData={this.props.doctorData} />
-
-        <div className="map-wrapper">
-          <div style={{ height: '100%', width: '100%' }}>
-            <GoogleMapReact 
-              bootstrapURLKeys={{ key: API_KEY_GEO }}
-              defaultCenter={
-                {
-                  lat: this.props.lat_long[0], 
-                  lng: this.props.lat_long[1]
-                }
-              }
-              defaultZoom={11}
-            >
-              <AnyReactComponent
-                lat={40.730610}
-                lng={-73.935242}
-                text="My Marker"
-              />
-            </GoogleMapReact>
-          </div>
-        </div>
-      </main>
-    );
-  }
+      <Map 
+        doctorData={props.doctorData} 
+        lat_long={props.lat_long}
+      />
+    </main>
+  );
 }
 
 export default Main;
