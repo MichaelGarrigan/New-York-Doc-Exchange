301c5e507c93261008b5c083bc88029756f3a569
M-garrigan <mgarrigan99@gmail.com>
2019-06-13 09:14:02 -0400

    Adding a simple spinner be a place holder while the app gets api data.
    The spinner is started in the Search component with the method attached
    to the submit button. When the method has the returned data from the
    controller the view changes and the animation is unmounted.

---
+++ src/components/Spinner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import '../styles/Spinner.css';
+
+const Spinner = () => (
+  <div className="spinner-flex">
+     <section className="spinner-wrapper">
+        <div className="spinner"></div>
+    </section>
+  </div>
+);
+
+export default Spinner;
