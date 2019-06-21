bc10a28042db0e0ecdc2445b2d209aaf74e63dd1
M-garrigan <mgarrigan99@gmail.com>
2019-06-20 20:05:51 -0400

    Adding the bulk of the landing page.
    The landing page is divided into three sections.
    First is an animated ticker banner and a call to action.
    Next is a three circle info section.
    And last is a chart and another call to action.

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,12 +1,78 @@
 import React from 'react';
 // import Spinner from './Spinner.js';
-// import '../styles/Spinner.css';
+import '../styles/Hero.less';
 
 const Hero = props => (
-  <section>
-    {/* <Spinner /> */}
-    <div className="hero-wrapper">Hero Page</div>
-  </section>
+  <main className="hero-wrapper">
+
+    <section className="hero-upper">
+      <div className="upper-flex-center">
+        <div className="upper-grid">
+          <div className="upper-grid-ticker">
+          
+          </div>
+          <div className="upper-grid-icon-building">
+          
+          </div>
+          <button className="upper-grid-call-to-action">
+            FIND A DOC
+          </button>
+        </div>
+      </div>
+    </section>
+
+    <section className="hero-middle">
+      <div className="flex-circles-center">
+
+        <div className="circles-wrapper">
+          <div className="circle-outline">
+            <div 
+              className="circle-icon icon-briefcase"
+            >
+            </div>
+          </div>
+          <div className="circle-text">
+            Intuitive advanced searching by symptoms, specialists, insurances or language.
+          </div>
+        </div>
+
+        <div className="circles-wrapper">
+          <div className="circle-outline">
+            <div 
+              className="circle-icon icon-stethoscope"
+            >
+            </div>
+          </div>
+          <div className="circle-text">
+            Utilizing data from the powerful Better Doctor API.
+          </div>
+        </div>
+
+        <div className="circles-wrapper">
+          <div className="circle-outline">
+            <div 
+              className="circle-icon icon-cloud-upload"
+            >
+            </div>
+          </div>
+          <div className="circle-text">
+            Find the right doctor and send the details and directions to you phone or email.
+          </div>
+        </div>
+
+      </div>
+    </section>
+
+    <section className="hero-lower">
+      <div className="lower-title">Find the right Doctor</div>
+      <div className="lower-wrapper">
+        <div className="lower-svg-chart-heart"></div>
+        <button className="lower-call-to-action">
+          Search Now
+        </button>
+      </div>
+    </section>
+  </main>
 );
 
 export default Hero;
