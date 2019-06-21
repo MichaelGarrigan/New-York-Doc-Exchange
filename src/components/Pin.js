453897b69b5622b187d5dea04432747d55bce527
M-garrigan <mgarrigan99@gmail.com>
2019-06-20 20:25:43 -0400

    Blocking .DS_Store

--- src/components/Pin.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import '../styles/Pin.css';
-
-// const Marker = ({ text }) => <div style={{ height: '10px', width: '10px', background: 'red', borderRadius: '50%' }}>{text}</div>;
-
-const Pin = () => <div className="pin"></div>;
-
-export default Pin;
