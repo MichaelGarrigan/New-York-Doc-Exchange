744e2b416fcbe83ae95d56fb169f6af408c0265d
M-garrigan <mgarrigan99@gmail.com>
2019-06-26 14:34:12 -0400

    Two files with data to pass to the Ticker component.
    The data is an object with a property called items that has a value
    of an array of objects. Each object is a line to include in the
    ticker text.

---
+++ src/server/helpers/ticker/ticker-1.js
@@ -0,0 +1,23 @@
+
+
+export default {
+  speed: '30s',
+  speedName: 'fast-ticker',
+  items: [
+    {
+      key: 111,
+      text: 'Find the right doctor',
+      color: '#a3c708'
+    },
+    {
+      key: 112,
+      text: 'Find the right doctor',
+      color: '#f9f9f9'
+    },
+    {
+      key: 113,
+      text: 'Find the right doctor',
+      color: 'rgb(250, 147, 12)'
+    }
+  ]
+}
---
+++ src/server/helpers/ticker/ticker-2.js
@@ -0,0 +1,23 @@
+
+
+export default {
+  speed: '50s',
+  speedName: 'slow-ticker',
+  items: [
+    {
+      key: 211,
+      text: 'Find the right doctor',
+      color: '#a3c708'
+    },
+    {
+      key: 212,
+      text: 'Find the right doctor',
+      color: '#f9f9f9'
+    },
+    {
+      key: 213,
+      text: 'Find the right doctor',
+      color: 'rgb(250, 147, 12)'
+    }
+  ]
+}
