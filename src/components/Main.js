fe46bd1740fa932709cfd3bdae2732bba8276f50
M-garrigan <mgarrigan99@gmail.com>
2019-06-27 13:24:53 -0400

    Moved the router and component selector to the App.js component.
    The Main component is a functional component that has one state hook
    which is capturing the first call to action on the landing page. The
    input element is just above the fold and will handle a quick search
    for the closest doctors by just entering a zip code.
    We should prompt the user that more advanced searching/filtering is
    available when they get the results on the Body component.:

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,75 +1,110 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
-import Body from './Body.js';
-import Hero from './Hero.js';
-import NavBar from './NavBar.js';
-import Search from './Search.js';
-import Spinner from './Spinner.js';
+import '../styles/Main.less';
+import Ticker from './Ticker.js';
+import fastTicker from '../server/helpers/ticker/ticker-1.js';
+import slowTicker from '../server/helpers/ticker/ticker-2.js';
+import Building from './svg/Building.js';
+import MagGlass from './svg/MagGlass.js';
 
-class Main extends Component {
-  state = {
-    mainView: 'hero',
 
-    doctorData: '',
-    lat_long: [40.730610, -73.935242],
-    clickedDoctor: ''
-  }
+const Main = props => {
 
-  switchMainView = view => {
-    console.log('view: ', view);
-    
-    this.setState({ mainView: view });
-  }
+  const [zipCode, setZipCode] = useState('');
+  
+  return (
+    <main>
 
-  setDoctorData = data => {
-    this.setState({
-      doctorData: data
-    });
-  }
+      <section className="upper">
+       
+          <div className="upper-grid">
+            <div className="upper-grid-ticker">
+              <Ticker tickerData={fastTicker}/>
+              <Ticker tickerData={slowTicker}/>
+            </div>
+      
+            <Building />
+            
+            <div className="upper-grid-cta">
+              <div className="upper-grid-cta-wrapper">
+                <input 
+                  className="upper-grid-cta-input" 
+                  min='10000' 
+                  max='99999'
+                  onChange={e => setZipCode(e.target.value) }
 
-  setLat_Long = coords => {
-    this.setState({
-      lat_long: coords
-    });
-  }
+                  placeholder='Enter a Zip Code'
+                  type='number'
+                  value={zipCode}
+                />
+                <div
+                  onClick={() => console.log('cta', zipCode)}
+                >
+                  <MagGlass className="upper-grid-svg-search" />
+                </ div>
+              </div>
+            </div>
+            
+          </div>
+      </section>
 
-  handleDocClick = index => {
-    this.setState({
-      clickedDoctor: index
-    });
-  }
+      <section className="hero-middle">
+        <div className="flex-circles-center">
 
-  render() {
-    
-    const threeComponents = {
-      hero: <Hero />,
-      spinner: <Spinner />,
-      search: 
-        <Search 
-          setDoctorData={this.setDoctorData}
-          setLat_Long={this.setLat_Long}
-          switchMainView={this.switchMainView}
-        />,
-      main: 
-        <Body
-          mainView={this.state.mainView}
-          switchMainView={this.switchMainView}
-          doctorData={this.state.doctorData} 
-          lat_long={this.state.lat_long}
-          handleDocClick={this.handleDocClick}
-          clickedDoctor={this.state.clickedDoctor}
-        />
-    }
+          <div className="circles-wrapper">
+            <div className="circle-outline">
+              <div 
+                className="circle-icon icon-briefcase"
+              >
+              </div>
+            </div>
+            <div className="circle-text">
+              Intuitive advanced searching by symptoms, specialists, insurances or language.
+            </div>
+          </div>
 
-    return (
-      <div className="nyde-wrapper">
-        <NavBar 
-          switchMainView={this.switchMainView}
-        />
-        { threeComponents[this.state.mainView] }
-      </div>
-    )
-  }
+          <div className="circles-wrapper">
+            <div className="circle-outline">
+              <div 
+                className="circle-icon icon-stethoscope"
+              >
+              </div>
+            </div>
+            <div className="circle-text">
+              Utilizing data from the powerful Better Doctor API.
+            </div>
+          </div>
+
+          <div className="circles-wrapper">
+            <div className="circle-outline">
+              <div 
+                className="circle-icon icon-cloud-upload"
+              >
+              </div>
+            </div>
+            <div className="circle-text">
+              Find the right doctor and send the details and directions to you phone or email.
+            </div>
+          </div>
+
+        </div>
+      </section>
+
+      <section className="hero-lower">
+        <div className="lower-title">Find the right Doctor</div>
+        <div className="lower-wrapper">
+          <div className="lower-svg-chart-heart"></div>
+
+          <Link to="/" className="link">
+            <button className="lower-call-to-action">
+              Search Now
+            </button>
+          </Link>
+        </div>
+      </section>
+    </main>
+  );
 }
 
 export default Main;
