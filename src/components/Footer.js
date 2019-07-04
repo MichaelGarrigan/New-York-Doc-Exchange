e53386ab0cd5760d2992a4da6ca619610502bbb6
M-garrigan <mgarrigan99@gmail.com>
2019-07-04 19:17:28 -0400

    The basic structure for a footer.
    The footer will contain some basic functionality, which will mainly
    be links to credit the apps author and to the Better Doctors API.

---
+++ src/components/Footer.js
@@ -0,0 +1,23 @@
+
+import React from 'react';
+
+import "../styles/Footer.less";
+
+const Footer = () => (
+  <div className="footer-wrapper">
+    <a href="#">
+      <p>Proudly Made By Michael Garrigan</p>
+    </a>
+    <a href="#">
+      <p>Other Works by Michael Garrigan</p>
+    </a>
+    <a href="#">
+      <p>View the Source Code</p>
+    </a>
+    <a href="#">
+      <p>Visit the Better Doctors API</p>
+    </a>
+  </div>
+);
+
+export default Footer;
