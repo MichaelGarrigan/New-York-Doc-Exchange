88413bc5f7b67398035991ea9c33799c7fab177d
M-garrigan <mgarrigan99@gmail.com>
2019-07-11 10:32:43 -0400

    Refactor layout of Insurances component and consolidateInsurance fn.
    Before we used a grid for each Insurance plan accepted this seemed
    to be overkill and so it was switched over to flex layout.
    
    The consolidateInsurance function aims to group the main insurance
    company with the sub policies together in the plans attribute.:

--- src/components/Insurances.js
+++ src/components/Insurances.js
@@ -1,50 +1,57 @@
-import React, { Fragment } from 'react';
-import constGridTemplateAreas from '../server/helpers/constGridTemplateAreas.js';
-import '../styles/Insurances.css';
+import React from 'react';
 
-const consolidateInsurances = list => {
-  let listToObject = {};
-  let newList = [];
+import '../styles/Insurances.less';
 
-  // here we are iterating over an array of objects
-  // we are placing the items into an object based on the name 
-  // the idea is to reduce the subcategories of insurance providers 
-  list.map(item => {
-    
-    if ( listToObject[item.insurance_provider.name] ) { // this name is in our object
-      listToObject[item.insurance_provider.name].insurance_plan.category.push(item.insurance_plan.category[0]);
-      listToObject[item.insurance_provider.name].insurance_plan.name.push(item.insurance_plan.name);
-      listToObject[item.insurance_provider.name].insurance_plan.uid.push(item.insurance_plan.uid);
-    } else { // this name is NOT in our object
-      listToObject[item.insurance_provider.name] = {
-        insurance_plan: {
-          category: item.insurance_plan.category,
-          name: [item.insurance_plan.name],
-          uid: [item.insurance_plan.uid]
-        },
-        insurance_provider: item.insurance_provider
-      }
-    }
-  })
-
-  // now we will convert the object back to an array
-  for (const item in listToObject) {
-    newList.push(listToObject[item]);
-  };
-  
-  return newList;
-};
-
-// this mutates the list into a alphabetically sorted array
+// alphabetically sort insurances (mutates 'list')
 const sortArray = list => {
-
-  list.sort( (a, b) => {
-    console.log('A: ', a.insurance_provider.name);
-    console.log('B: ', b.insurance_provider.name);
-    return a.insurance_provider.name - b.insurance_provider.name; 
+  list.sort( (a,b) => {
+    const x = a.name;
+    const y = b.name;
+    if (x < y) return -1;
+    if (x > y) return 1;
+    return 0;
   });
   
   return list;
+};
+
+const consolidateInsurances = list => {
+
+  // list an array of objects
+  // Here we want one provider with all the subcategories in 'plan' 
+  let arrToObj = {};
+  list.forEach(item => {
+    const name = item.insurance_provider.name;
+
+    if ( arrToObj[name] ) { // this name is in our object
+      arrToObj[name].plans.push(
+        {
+          category: item.insurance_plan.category[0],
+          name: item.insurance_plan.name
+        }
+      ) 
+    } else { // this name is NOT in our object
+      arrToObj[name] = {
+        plans: [
+          {
+            category: item.insurance_plan.category[0],
+            name: item.insurance_plan.name
+          }
+        ]
+      }
+    }
+  });
+  
+  // Convert the object back to an array
+  let newList = [];
+  for (const item in arrToObj) {
+    newList.push({ 
+      name: item,
+      plans: arrToObj[item].plans
+    });
+  };
+  
+  return sortArray(newList);
 };
 
 
@@ -56,45 +63,31 @@ const Insurances = ({ insurances }) => {
 
   // consolidate insurances
   const newList = consolidateInsurances(insurances);
-  console.log('newList Before: ', newList);
-  // alphabetically sort insurances
-  sortArray(newList);
-  console.log('newList: ', newList);
-
+  
   return (
-    newList.map( (item, idx) => {
-      const len = item.insurance_plan.category.length;
-      
-      return (
-        <div className="doctor-insurance-wrapper">
-          <div 
-            className="doctor-insurance-grid"
-            style={{
-              gridTemplateAreas: `${constGridTemplateAreas[len + ""]}`,
-              gridTemplateRows: `repeat(${len + 1}, 1fr)`
-            }}
-          >
-            <div className="doctor-provider">
-              {item.insurance_provider.name}
-            </div>
-           {
-             item.insurance_plan.category.map( (cat, i) => {
-              return (
-                <Fragment key={i} >
-                  <div 
-                    className={`${cat === 'medical' ? 'medical-icon' : 'dental-icon'} icon${i}`}
-                  ></div>
-                  <div className={`doctor-plan plan${i}`}>
-                    {item.insurance_plan.name[i]}
-                  </div>
-                </Fragment>
-               )
-             })
-           }
+    newList.map( item => (
+        <div className="insurance-wrapper" key={item}>
+          
+          <div className="insurance-provider">
+            {item.name}
           </div>
+          {
+            item.plans.map( plan => (
+              <div className="insurance-plan-flex" key={plan.name}>
+                {
+                  plan.category === 'medical'
+                    ? <div className="med-icon"></div>
+                    : <div className="den-icon"></div>
+                }
+                <div className="insurance-plan-name">
+                  {plan.name}
+                </div>
+              </div>
+            ))
+          }
         </div>
-      );
-    })
+      )
+    )
   )
 };
 
