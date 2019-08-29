c8ca196552f5f83537647eef6fce54849a2027d8
M-garrigan <mgarrigan99@gmail.com>
2019-08-29 09:03:30 -0400

    Adding another div wrapper

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,23 +4,20 @@ import { Link } from 'react-router-dom';
 import MagGlass from './svg/MagGlass.js';
 import '../styles/NavBar.less';
 
-const NavBar = () => (
+export default () => (
     <nav className="navbar-wrapper">
+      <div className="navbar-blue">
+        <Link to="/" className="link-nav">
+          <div className="nav-title">New York Doc Exchange</div>
+        </Link>
 
-      <Link to="/" className="link-nav">
-        <div className="nav-title">New York Doc Exchange</div>
-      </Link>
-
-      <Link 
-        to="/search" 
-        className="link-nav link-nav-search"
-      >
-        <MagGlass className="icon-search" />
-        <span>Search</span>
-      </Link>
-
+        <Link 
+          to="/search" 
+          className="link-nav link-nav-search"
+        >
+          <MagGlass className="icon-search" />
+          <span>Search</span>
+        </Link>
+      </div>
     </nav>
 );
-
-
-export default NavBar;
