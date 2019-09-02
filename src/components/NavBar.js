21110b95dbbb26dad3c0bbd6d38aa5de94ea16a9
M-garrigan <mgarrigan99@gmail.com>
2019-09-02 12:40:59 -0400

    Replacing the static title with a react svg component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,13 +2,26 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import MagGlass from './svg/MagGlass.js';
+import NydeFullTitle from './svg/NydeFullTitle.js';
+
 import '../styles/NavBar.less';
 
-export default () => (
+export default props => {
+  const width = props.dimensions.width;
+  const titleWidth = Math.round(width * 0.7);
+  const titleHeight = Math.round(titleWidth * 0.1);
+
+  return (
     <nav className="navbar-wrapper">
-      <div className="navbar-blue">
+      {/* <div className="navbar-blue"> */}
         <Link to="/" className="link-nav">
-          <div className="nav-title">New York Doc Exchange</div>
+          <NydeFullTitle
+            className="icon-nydefulltitle"
+            height={titleHeight}
+            width={titleWidth}
+          />
+          {/* <div className="nav-title">New York Doc Exchange</div> */}
+          {/* <div className="nav-title"></div> */}
         </Link>
 
         <Link 
@@ -18,6 +31,7 @@ export default () => (
           <MagGlass className="icon-search" />
           <span>Search</span>
         </Link>
-      </div>
+      {/* </div> */}
     </nav>
-);
+  );
+};
