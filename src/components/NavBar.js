30c745fec4d207757f640952e2180dbff819f70e
M-garrigan <mgarrigan99@gmail.com>
2019-09-19 15:17:07 -0400

    Removing the search button and moving it further down the page

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,37 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import MagGlass from './svg/MagGlass.js';
 import NydeFullTitle from './svg/NydeFullTitle.js';
 
 import '../styles/NavBar.less';
 
-export default props => {
-  const width = props.dimensions.width;
-  const titleWidth = Math.round(width * 0.7);
-  const titleHeight = Math.round(titleWidth * 0.1);
+export default ({ width }) => {
+  
+  const svgWidth = Math.round(width * 0.9);
+  const svgHeight = Math.round(svgWidth * 0.1);
 
   return (
     <nav className="navbar-wrapper">
-      {/* <div className="navbar-blue"> */}
+  
         <Link to="/" className="link-nav">
           <NydeFullTitle
             className="icon-nydefulltitle"
-            height={titleHeight}
-            width={titleWidth}
+            height={svgHeight}
+            width={svgWidth}
           />
-          {/* <div className="nav-title">New York Doc Exchange</div> */}
-          {/* <div className="nav-title"></div> */}
         </Link>
 
-        <Link 
-          to="/search" 
-          className="link-nav link-nav-search"
-        >
-          <MagGlass className="icon-search" />
-          <span>Search</span>
-        </Link>
-      {/* </div> */}
     </nav>
   );
 };
