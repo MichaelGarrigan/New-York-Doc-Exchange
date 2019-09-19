b3aa882d68a65d0f24721223156b61af5961d640
M-garrigan <mgarrigan99@gmail.com>
2019-09-19 15:07:16 -0400

    Combined fastTicker and slowTicker data files into one

---
+++ src/server/tickerText.js
@@ -0,0 +1,45 @@
+
+export default {
+  fastTicker: {
+    speed: '30s',
+    speedName: 'fast-ticker',
+    items: [
+      {
+        key: 111,
+        text: 'Find the right doctor, right now.',
+        color: '#a3c708'
+      },
+      {
+        key: 112,
+        text: 'Better Health is one click away.',
+        color: '#f9f9f9'
+      },
+      {
+        key: 113,
+        text: 'Servers are standing by to help you :)',
+        color: 'rgb(250, 147, 12)'
+      }
+    ]
+  },
+  slowTicker: {
+    speed: '40s',
+    speedName: 'slow-ticker',
+    items: [
+      {
+        key: 211,
+        text: 'NYDE -- ^ 4.6%',
+        color: '#a3c708'
+      },
+      {
+        key: 212,
+        text: 'BCBS -- ^ 2.4%',
+        color: '#f9f9f9'
+      },
+      {
+        key: 213,
+        text: 'Find the right doctor',
+        color: 'rgb(250, 147, 12)'
+      }
+    ]
+  }
+};
