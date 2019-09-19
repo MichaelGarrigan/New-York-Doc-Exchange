63ad1629e99b0452d95c99dffe87077e2595bd89
M-garrigan <mgarrigan99@gmail.com>
2019-09-19 15:18:37 -0400

    Cleaning up comments and whitespace

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,18 +4,18 @@ import React from 'react';
 import "../styles/Footer.less";
 
 export default () => (
-  <div className="footer-wrapper">
-    <a href="#">
-      <p>Proudly Made By Michael Garrigan</p>
-    </a>
-    <a href="#">
-      <p>Other Works by Michael Garrigan</p>
-    </a>
-    <a href="#">
-      <p>View the Source Code</p>
-    </a>
-    <a href="#">
-      <p>Visit the Better Doctors API</p>
-    </a>
-  </div>
+    <div className="footer-wrapper">
+      <a href="#">
+        <p>Proudly Made By Michael Garrigan</p>
+      </a>
+      <a href="#">
+        <p>Other Works by Michael Garrigan</p>
+      </a>
+      <a href="#">
+        <p>View the Source Code</p>
+      </a>
+      <a href="#">
+        <p>Visit the Better Doctors API</p>
+      </a>
+    </div>
 );
